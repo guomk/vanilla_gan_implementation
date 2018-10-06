d408b3dc64b9b1519a967e8851eed19d63a9de9b
Mukun Guo <gmk19990312@gmail.com>
2018-10-06 16:53:09 +0800

    added support for matplotlib

--- gan_2lp.py
+++ gan_2lp.py
@@ -3,9 +3,11 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import matplotlib
+matplotlib.use('Agg')
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 
 def plot(samples):
